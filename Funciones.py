275fcd1d06bc65e7e6731106247b3c3dccf54bda
Nicolás <con23197@uvg.edu.gt>
2024-11-11 17:15:45 -0600

    4 funciones de 6

--- Funciones.py
+++ Funciones.py
@@ -1,5 +1,6 @@
 #Generar primo aleatorio
 import random as r
+import numpy as np
 def generar_primo(rango_inferior, rango_superior):
       while True:
          numero = r.randint(rango_inferior, rango_superior)
@@ -14,22 +15,96 @@ def generar_primo(rango_inferior, rango_superior):
 def mcd(a, b):
    #Se verifica que los valores ingresados sean positivos
    if a <= 0 or b <= 0:
-      print("Los valores ingresados tienen que ser positivos")
-      exit()
+      raise ValueError("Los valores ingresados tienen que ser positivos")
+   
    #Se intercambian los valores si a < b
    if a < b:
       a, b = b,a
-      print("Se han intercambiado los valores: ahora a = " + str(a) + " y b = " + str(b))
+   
+   #Ecuación de Euclides
    else:
       while b != 0:  # Mientras b no sea 0 se ejecuta el ciclo
          r = a % b # Se calcula el residuo de la división de a entre b
          a = b # Se asigna el valor de b a a
          b = r # Se asigna el valor de r a b
-      return a # Se retorna el valor de a
+   return a # Se retorna el valor de a
 
-#def inverso_modular(e,n):
+def inverso_modular(e,n):
+   #verificar que sean positivos
+   if e <=0 or n <=0:
+      raise ValueError("Los valores ingresados tienen que ser positivos")
+   
+   #Verificamos que el mcd sea 1
+   if mcd(e,n)!=1:
+      raise ValueError("Los valores ingresados no son coprimos, por lo tanto no tienen inverso modular")
+   
+   #Algoritmo extendido de Euclides
+   c = e
+   d = n
+   #Matriz identidad
+   m = np.array([
+         [1,0],
+         [0,1]
+   ])
 
-#def generar_llaves(rango_inferior, rango_superior):
+
+   while d != 0:
+      q = c//d #División entera
+      r = c%d #Residuo
+      c = d #Se asigna el valor de d a c
+      d = r #Se asigna el valor de r a d
+
+      #Matriz de transformación
+      Q1 = np.array([
+         [0,1],
+         [1,-q]
+      ])
+
+      #Multiplicación de matrices
+      m = Q1@m
+
+   x = m[0][0]
+
+   if x < 0:
+      x = (x+n)%n
+
+   return x
+
+def generar_llaves(rango_inferior, rango_superior):
+   #Generar primos p y q
+   p = generar_primo(rango_inferior, rango_superior)
+   q = generar_primo(rango_inferior, rango_superior)
+
+   #verificar que p y q sean diferentes
+   while p == q:
+      q = generar_primo(rango_inferior, rango_superior)
+
+   #Calcular n
+   n = p*q
+
+   #Calcular phi
+   phi = (p-1)*(q-1)
+
+   #Generar e aleatorio entre 1 < e < phi y el mcd(e,phi) == 1
+   e = r.randint(2,phi - 1)
+   while mcd(e,phi)!=1:
+      e = r.randint(2,phi - 1)
+   
+   #Calcular d el inverso modular de e
+   d = inverso_modular(e,phi)
+
+   if d == None:
+      e = r.randint(2,phi - 1)
+      while mcd(e,phi)!=1:
+         e = r.randint(2,phi - 1)
+      d = inverso_modular(e,phi)
+   
+   #Llaves
+   llave_publica = (n,e)
+   llave_privada = (n,d)
+
+   #Se retornan las llaves
+   return llave_publica and llave_privada
 
 #def encriptar(caracter, llave_publica):
 
